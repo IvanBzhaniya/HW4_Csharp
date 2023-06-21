c2cedc60526e75e32daeee98de300139ef9083f8
IvanBzhaniya <ivan.bzhaniya@gmail.com>
2023-06-21 19:37:53 +0300

    ex3

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 
-*/
-
 
 
 Console.WriteLine("input number1");
@@ -28,3 +26,47 @@ else
 {
     Console.WriteLine("must be > 0");
 }
+
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+
+
+Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int temp1 = num%10;
+int temp2 = num%100/10;
+int temp3 = num/100;
+int sum = temp1+temp2+temp3;
+Console.WriteLine(sum);
+
+
+
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
+*/
+
+int [] CreateNewArray (int size_array)
+{
+    int [] random1array = new int[size_array];
+    for (int i = 0; i < size_array; i++)
+    {
+        random1array[i] = new Random().Next(0, 100);
+    }
+    return random1array;
+}
+
+void PrintArray (int [] array_to_print)
+{
+    Console.WriteLine("your array is ");
+    for (int i = 0; i < array_to_print.Length; i++)
+        Console.Write(array_to_print[i] + " ");
+}
+int size = 8;
+int [] firstArray = CreateNewArray(size);
+PrintArray(firstArray);
+
